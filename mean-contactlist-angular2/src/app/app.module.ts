2fd76c7430982c49d6bfee57e432294b5c04509f
Hector Ramirez <hramirez@bofifederalbank.com>
2019-03-06 16:19:08 -0600

    fixed IE polyfills issues

--- mean-contactlist-angular2/src/app/app.module.ts
+++ mean-contactlist-angular2/src/app/app.module.ts
@@ -1,17 +1,26 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MatTableModule } from '@angular/material/table';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { ContactDetailsComponent } from "./contacts/contact-details/contact-details.component";
-import { ContactListComponent } from "./contacts/contact-list/contact-list.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [AppComponent, ContactDetailsComponent, ContactListComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    MatTableModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
